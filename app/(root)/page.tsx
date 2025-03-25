ebc82706aee1d9629acf5000eb4a337d4b0baafb
Dave23x1 <ryandavedonos@gmail.com>
2025-03-25 11:24:42 +0800

    Chore: Add Services Component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,11 @@
 import Banner from "@/components/banner";
-
+import Services from "@/components/services";
 const page = () => {
   return (
     <>
       <Banner number={1} className="w-[500px] text-[50px]" />
       <Banner number={0} className="w-[500px] text-[24px]" />
+      <Services />
     </>
   );
 };
---
+++ app/(root)/services/page.tsx
@@ -0,0 +1,9 @@
+import Services from "@/components/services";
+
+export default function ServicesAndFeautures() {
+  return (
+    <div>
+      <Services />
+    </div>
+  );
+}
